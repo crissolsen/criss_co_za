84f58dd5c71590c15aa26a237664ba607245dbde
Christopher <chris@homecode.co.za>
2020-07-10 11:14:10 +0200

    Restyle and add greensock

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ $(".grid-container").hide()
 
 $(".no-space").click(function() {
     $("#navbar").hide();
+    exciting();
     $(".grid-container").fadeIn("fast");
 })
 
+
+gsap.from("#navbar", { duration: 1.3, y: '-1000px', opacity: 0, ease: 'bounce'})
+
+function exciting() {
+    gsap.from(".grid-item", { duration: 0.6, opacity: 0, stagger: 0.5})
+}
